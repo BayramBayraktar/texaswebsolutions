0c7cea493a0a3eb5e748aeffc307d1aa64f96310
Bayram Bayraktar <105519834+BayramBayraktar@users.noreply.github.com>
2025-03-06 16:25:56 +0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,30 +1,33 @@
-import { Html, Head, Main, NextScript } from "next/document";
-
 export default function Document() {
   return (
     <Html lang="en">
-      <link rel="icon"
-        href="/images/-TWS-.png"
-        type="image/x-icon"
-      />
-      <meta name="trustpilot-one-time-domain-verification-id" content="97da5b14-94e8-43a9-bfbb-a691e9136b74"/>
-      {/* google search console */}
-      <meta name="google-site-verification" content="AXO1SCzCW_G1LbqQ8UO8nAwl5lJfj61p-EhaIXp-mxs" />
-      <link
-        rel="stylesheet"
-        type="text/css"
-        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-      />
-      <link
-        rel="stylesheet"
-        type="text/css"
-        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-      />
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
-      />
-      <Head />
+      <Head>
+        <link rel="icon"
+          href="/images/-TWS-.png"
+          type="image/x-icon"
+        />
+        <meta name="trustpilot-one-time-domain-verification-id" content="97da5b14-94e8-43a9-bfbb-a691e9136b74"/>
+        <meta name="google-site-verification" content="AXO1SCzCW_G1LbqQ8UO8nAwl5lJfj61p-EhaIXp-mxs" />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+        />
+        <Script
+          src="https://analytics.ahrefs.com/analytics.js"
+          data-key="ejTVpWX8TsE6HvnQTooJeA"
+          strategy="afterInteractive"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
